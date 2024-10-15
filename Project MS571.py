834675c458c6efa671b601079dbc28a9eb45f95a
Felipe Basilio <105984768+Felipe-Basilio@users.noreply.github.com>
2024-10-15 12:13:18 -0300

    Update Project MS571.py

--- Project MS571.py
+++ Project MS571.py
@@ -2,8 +2,36 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
+import openpyxl
 
-economia = pd.read_csv("economy.csv")
+players = pd.read_csv("players.csv")
 
-print(economia)
- 
+picks = pd.read_csv("picks.csv")
+
+resultados = pd.read_csv("results.csv")
+
+#######
+
+#Avaliação dados - Players
+
+print(players.head())
+
+print(players.info())
+
+print(players.describe())
+
+#Avaliação dados - Picks
+
+print(picks.head())
+
+print(picks.info())
+
+print(picks.describe())
+
+#Avaliação dados - Results
+
+print(resultados.head())
+
+print(resultados.info())
+
+print(resultados.describe())
