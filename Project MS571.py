df81e6c887b731d73660b5434b15721679de2c8a
Felipe Basilio <105984768+Felipe-Basilio@users.noreply.github.com>
2024-10-23 01:12:06 -0300

    Update Project MS571.py

--- Project MS571.py
+++ Project MS571.py
@@ -3,35 +3,33 @@ import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 import openpyxl
+import matplotlib.pyplot as plt
 
-players = pd.read_csv("players.csv")
+dados = pd.read_csv("Cartão de Crédito/creditcard.csv")
 
-picks = pd.read_csv("picks.csv")
+def split_train_val_test(data, test_ratio, val_ratio, random_state=42):
+    np.random.seed(random_state)  # Fixa a seed para reprodutibilidade
+    shuffled_indices = np.random.permutation(len(data))
 
-resultados = pd.read_csv("results.csv")
+    test_set_size = int(len(data) * test_ratio)
+    val_set_size = int(len(data) * val_ratio)
 
-#######
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    val_indices = shuffled_indices[test_set_size:test_set_size + val_set_size]
 
-#Avaliação dados - Players
+    return data.iloc[train_indices], data.iloc[val_indices], data.iloc[test_indices]
 
-print(players.head())
+train_set, val_set, test_set = split_train_val_test(dados, val_ratio=0.2, test_ratio=0.2)
 
-print(players.info())
+# Salvar os conjuntos de treino, validação e teste em arquivos CSV
+train_set.to_csv('train_set.csv', index=False)
+val_set.to_csv('val_set.csv', index=False)
+test_set.to_csv('test_set.csv', index=False)
 
-print(players.describe())
+#Modelo de Regressão Linear
 
-#Avaliação dados - Picks
+from sklearn.linear_model import LinearRegression
 
-print(picks.head())
-
-print(picks.info())
-
-print(picks.describe())
-
-#Avaliação dados - Results
-
-print(resultados.head())
-
-print(resultados.info())
-
-print(resultados.describe())
+lin_reg = LinearRegression()
+lin_reg.fit()
